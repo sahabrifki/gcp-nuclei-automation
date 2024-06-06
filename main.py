7251688da1836c102124b168a3aff3aaa29644b7
Muhammad Rifki <duniaaguaa@gmail.com>
2024-06-06 10:36:44 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from slack_sdk import WebClient
 import requests
 import psutil
 import time
-import sqlite3
+import psycopg2
 
 
 current_date = date.today()
@@ -14,11 +14,8 @@ now_time = datetime.now()
 current_time = now_time.strftime("%H:%M:%S")
 #print("Current time Launch ", current_time)
 
-message = f"Hi Infosec Team, automated scanning for today {current_date} completed at {current_time}"
-notif_launch_messages = f"Hi Infosec Team, automated scanning for today {current_date} is starting at {current_time}"
-slack_token = ""
 
-
+#listing all projects and enumerate all exeternal ip address from each project
 def list_external_ips():
     external_ips = []
     # Get a list of all projects
@@ -36,69 +33,31 @@ def list_external_ips():
         if ips_json:
             for x in ips_json:
                 if (x["addressType"] == "EXTERNAL"):
-                    print ("(+)External IP addresses found: " + x["address"])
+                    #print ("(+)External IP addresses found: " + x["address"])
                     external_ips.append(x["address"])
 
     return external_ips
 
-def write_array_to_file():
-    external_ips = list_external_ips() 
-    filename = "/home/muhamadrifki/nuclei_project/nuclei_project/webview/list_ip_external.txt"
+#execute nuclei to initiate scanning
+def run_nuclei():    
+    path_to_file = "/home/path/nuclei_project/nuclei_project/webview/nuclei_output_json/"
+    command = ["nuclei" , "-l", "/home/path/nuclei_project/nuclei_project/webview/target/list_ip_external_" + tanggal + "_" + waktu + ".txt", "-c", "9", "-pc", "9", "-je", path_to_file + "nuclei_output_" + tanggal + "_" + waktu + ".json"]
+    #print(command)
     
-    try:
-        with open(filename, 'w') as file:
-            for ip in external_ips:
-                file.write(str(ip) + '\n')
-        print(f"Values written to {filename} successfully!")
-    except IOError:
-        print(f"Error writing to {filename}")
-
-
-
-#notified nuclei running to slack
-def notif_launch():
-    client = WebClient(token=slack_token)
-    tanggal = date.today()
-    hours = datetime.now().strftime("%H:%M:%S")
-    notif_messages = f"Hi Infosec Team, automated scanning for today {tanggal} is starting at {hours}" + "\n" + "template-concurrency : 9 templates/request" + "\n" + "request concurrency: 9 request/s"
-
-# Send a message
-    client.chat_postMessage(
-        channel="va-automation", 
-        text=notif_messages, 
-        username="botvaautomation"
-    )
-
-
-
-#execute nuclei scanning
-def run_nuclei():
-    global waktu, tanggal
-    waktu = str(current_time)
-    tanggal = str(current_date)
-    #print(tanggal, waktu)
-    # Command to run nuclei
-    path_to_file = "/home/muhamadrifki/nuclei_project/nuclei_project/webview/nuclei_output_json/"
-    command = ["nuclei" , "-l", "/home/muhamadrifki/nuclei_project/nuclei_project/webview/list_ip_external.txt", "-c", "9", "-pc", "9", "-je", path_to_file + "nuclei_output_" + tanggal + "_" + waktu + ".json"]
-    print(command)
-    #command = ["nuclei" , "-u", "hahhahaha.x", "-je" , "nuclei_output_" + tanggal + "_" + waktu + ".json"]
-    
-    
-
     # Start the subprocess
     process = subprocess.Popen(command,stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-    print("Running Nuclei at : ", tanggal, waktu)
+    print("(+) Running Nuclei at : ", tanggal, waktu)
     process.communicate()
    
 
-
+#detect all process in linux, to detect nuclei prosess
 def find_process_with_command(command):
     for proc in psutil.process_iter(['pid', 'cmdline']):
         if proc.info.get('cmdline') and command in ' '.join(proc.info['cmdline']):
             return proc.info['pid']
     return None
 
-
+# Informing users from process iteration and notified if nuclei is done
 def detect_process():
     command_to_find = "nuclei -l list_ip_external.txt"
     #time.sleep(10)
@@ -107,16 +66,22 @@ def detect_process():
     while pid:
         pid = find_process_with_command(command_to_find)
     
-    print("Nuclei Scan Finish, saving result to database .... .... ....")
-    #sending_to_slack()
+    print("[INFO] Nuclei Scan Finish, Parsing Data Into Database .... .... ....")
+    sending_to_slack()
 
-
+#Reading Nuclei with JSON Output
 def read_file():
-    #current_date = str(current_date)
-    # current_time = str(current_time)
-    #print(tanggal, waktu)
+    conn = psycopg2.connect(
+        host = "localhost",
+        database = "automation_db",
+        user = "automation",
+        password = "PASSWORD" 
+    )
+
+    cursor = conn.cursor()
 
     filepath =("/home/muhamadrifki/nuclei_project/nuclei_project/webview/nuclei_output_json/nuclei_output" + "_" + tanggal + "_" + waktu + ".json")
+    #filepath =("/home/muhamadrifki/nuclei_project/nuclei_project/webview/nuclei_output_json/nuclei_output_2024-05-30_13:57:34.json")
     with open(filepath, 'r') as file:
         mentahan = file.read()
         #print("success retrieving latest json result")
@@ -135,58 +100,33 @@ def read_file():
         "timestamp": item.get("timestamp", None)
         }
         filtered_data.append(filtered_item)
-
-# Displaying filtered data
+        json_data = json.dumps(filtered_data)
+        # return json_data
+    
     for item in filtered_data:
-        json_dumps = json.dumps(item)
-        return json_dumps
-        #fixed_data = json_dumps.split(",")
-        #print(json_dumps)
-    
-def parsingSqlite():
-    print("parsing data to sqlite database....")
-    json_dumps = read_file()
-    json_data = json.loads(json_dumps)  # Parse the JSON string to a dictionary
+        for key in item:
+            if item[key] is None:
+                item[key] = "None"
+        
+        cursor.execute("""
+            INSERT INTO scanResult (name, description, reference, severity, host, ip, curlCommand, timestamp)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+        """, (
+            item["name"],
+            item["description"],
+            item["reference"],
+            item["severity"],
+            item["host"],
+            item["ip"],
+            item["curl-command"],
+            item["timestamp"]
+        ))
 
-    conn = sqlite3.connect('automation.db')
-    cursor = conn.cursor()
-
-    # Insert data into the table
-    insert_query = '''
-    INSERT INTO scanResult (name, description, reference, severity, host, ip, curlCommand, timestamp)
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-    '''
-
-    data_tuple = (
-        str(json_data['name']),
-        str(json_data['description']),
-        str(json.dumps(json_data['reference'])),  # Convert list to JSON string
-        str(json_data['severity']),
-        str(json_data['host']),
-        str(json_data['ip']),  # Convert None to string
-        str(json_data['curl-command']),  # Convert None to string
-        str(json_data['timestamp'])
-    )
-
-    
-
-    cursor.execute(insert_query, data_tuple)
     conn.commit()
-
-    # Close the connection
+    print("[FINISH] Data Successfully Stored into database")
+    cursor.close()
     conn.close()
-    time.sleep(10)
-    print("data successfully saved into automation database (automation.db)")
-
-    
-    
-
-
         
-        
-
-    
-
     
 
 def sending_to_slack():
@@ -194,10 +134,7 @@ def sending_to_slack():
     #print("current time after scan :", current_time)
     tanggal = date.today()
     hours = datetime.now().strftime("%H:%M:%S")
-    notif_messages = f"Hi Infosec Team, automated scanning for today {tanggal} completed at {hours} and all scan result available to see at http://34.34.223.15:9001/"
-
-
-
+    notif_messages = f"Hi Infosec Team, automated scanning for today {tanggal} completed at {hours} and all scan result available to see at http://34.34.223.15:8080/"
 #Send a message
     client.chat_postMessage(
         channel="va-automation", 
@@ -207,17 +144,66 @@ def sending_to_slack():
 
 
 
-
-
-
 def main():
-    #notif_launch()
-    write_array_to_file()
-    run_nuclei()
-    time.sleep(10)
-    detect_process()
-    #read_file()
-    parsingSqlite()
+    global waktu, tanggal
+    waktu = str(current_time)
+    tanggal = str(current_date)
+   
+    external_ips = list_external_ips() 
+    filename = "/home/muhamadrifki/nuclei_project/nuclei_project/webview/target/list_ip_external.txt"
+    new_target = f"/home/muhamadrifki/nuclei_project/nuclei_project/webview/target/list_ip_external_{tanggal}_{waktu}.txt"
+    
+    try:
+        with open(filename, 'r') as file:
+            existing_ips = file.read().splitlines()
+        
+        new_ips = [ip for ip in external_ips if ip not in existing_ips]
+        
+        if not new_ips:
+            print("All IP addresses are identic with precious IP Lists collection result, Nuclei Scan Aborted")
+            client = WebClient(token=slack_token)
+            client.chat_postMessage(
+                channel="va-automation",
+                text = "Hi Infosec Team, No New IP Address Found From Previous Asset Collection Result, Nuclei Scan Abort",
+                username="botvaautomation"
+            )
+        else:
+            with open(filename, 'a') as oldFile:
+                for ip in new_ips:
+                    oldFile.write(ip + '\n')
+            
+            for ip in new_ips:
+                print("New Ip Addresses Found: ", ip)
+            
+            with open(new_target, 'a') as newFile:
+                for ip in new_ips:
+                    newFile.write(ip + '\n')
+                    
+
+            
+            print(f"[Saving Files] - New values appended to {new_target} successfully!")
+
+            with open(new_target, 'r' ) as newIP:
+                new_ip = newIP.read().splitlines()
+                ip_to_string ="\n".join(str(x) for x in new_ip)
+                
+                #print("new_ip: " , ip_to_string)
+            
+            client = WebClient(token=slack_token)
+            client.chat_postMessage(
+            channel="va-automation",
+            text = f"Hi Infosec Team, New IP Address detected : \n {ip_to_string}" +  "\n" + "\n" + "Nuclei Scan for :" + "\n" f"{ip_to_string}" + "\n" + f"Started at {tanggal, waktu}",
+            username="botvaautomaton"
+             )
+            
+            run_nuclei()
+            time.sleep(10)
+            detect_process()
+            read_file()
+
+            
+    except IOError:
+        print(f"Error reading from {filename}, {oldFile}")
     
 
 
